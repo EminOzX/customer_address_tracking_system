c0cbdeab3dd094ec9c9b2e59a44707af047d92a5
Emin Öz <153459728+EminOzX@users.noreply.github.com>
2025-12-12 21:14:32 +0300

    Change varchar to nvarchar in database schemas and some problems fixed.

--- proje.sql
+++ proje.sql
@@ -6,20 +6,20 @@ GO
 
 create table roles (
     role_id INT IDENTITY(1,1) primary key,
-    role_name varcHar(50)
+    role_name nnvarchar(50)
 );
 GO
 
 create table users (
     user_id INT IDENTITY(1,1) primary key,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
+    first_name nnvarchar(50) NOT NULL,
+    last_name nvarchar(50) NOT NULL,
     role_id INT NOT NULL,
-    created_at varchar(30),
-    username varchar(50) NOT NULL UNIQUE,
-    email varchar(50) NOT NULL UNIQUE,
-    [password] varchar(50) NOT NULL,
-    phone varchar(50) NOT NULL UNIQUE,
+    created_at nvarchar(30),
+    username nvarchar(50) NOT NULL UNIQUE,
+    email nvarchar(50) NOT NULL UNIQUE,
+    [password] nvarchar(50) NOT NULL,
+    phone nvarchar(50) NOT NULL UNIQUE,
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 GO
@@ -33,13 +33,13 @@ GO
 
 create table cities (
     city_id INT IDENTITY(1,1) primary key,
-    city_name nvarchar(50)
+    city_name nnvarchar(50)
 );
 GO
 
 create table districts (
     district_id INT IDENTITY(1,1) primary key,
-    district_name nvarchar(50),
+    district_name nnvarchar(50),
     city_id INT,
     FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
@@ -48,10 +48,10 @@ GO
 create table address (
     address_id INT IDENTITY(1,1) primary key,
     customer_id INT,
-    address_type NVARCHAR(20),
-    city_id nvarchar(50),
+    address_type Nnvarchar(20),
+    city_id nnvarchar(50),
     district_id INT,
-    address_description nvarchar(255),
+    address_description nnvarchar(255),
     building_no INT,
     apartment_no INT,
     postal_code INT,
@@ -64,10 +64,10 @@ GO
 
 create table products (
     product_id INT IDENTITY(1,1) primary key,
-    product_name varchar(50),
+    product_name nvarchar(50),
     vendor_id INT,
     quantity INT,
-    category VARCHAR(20),
+    category nvarchar(20),
     unit_price INT,
     total_price INT
 );
@@ -75,7 +75,7 @@ GO
 /*
 create  table branchs (
     branch_id INT primary key,
-    branch_name varchar(50),
+    branch_name nvarchar(50),
     address_id INT,
     FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
@@ -91,8 +91,8 @@ GO
 create table contacts (
     contact_id INT primary key,
     user_id INT,
-    phone varchar(13),
-    --email varchar(50)
+    phone nvarchar(13),
+    --email nvarchar(50)
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 GO
@@ -101,21 +101,21 @@ create table orders (
     order_id INT IDENTITY(1,1) primary key,
     customer_id INT,
     address_id INT,
-    branch_id INT,
-    order_status VARCHAR(20),
-    --payment_method VARCHAR(20),  
+    vendor_id INT,
+    order_status nvarchar(20),
+    --payment_method nvarchar(20),  
     quantity INT,
     total_price DECIMAL(10, 2),
-    created_at VARCHAR(50),
+    created_at nvarchar(50),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
     FOREIGN KEY (address_id) REFERENCES address(address_id),
-    FOREIGN KEY (branch_id) REFERENCES branchs(branch_id)
+    FOREIGN KEY (vendor_id) REFERENCES vendors(vendor_id)
 );
 GO
 
 create table vendors (
     vendor_id INT IDENTITY(1,1) primary key,
-    vendor_name varchar(50),
+    vendor_name nvarchar(50),
 );
 GO
 /*
@@ -123,8 +123,8 @@ create table shippings (
     shipping_id INT primary key,
     order_id INT NOT NULL,
     courier_id INT NOT NULL,
-    tracking_number VARCHAR(20) NOT NULL,
-    shipping_status VARCHAR(20) NOT NULL
+    tracking_number nvarchar(20) NOT NULL,
+    shipping_status nvarchar(20) NOT NULL
         CONSTRAINT CK_SHIPPINGS_STATUS CHECK (shipping_status IN ('preparing','on_the_way','delivered')),
     shipped_at DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
     delivered_at DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
@@ -139,7 +139,7 @@ create table sales_logs (
     order_id INT,
     product_id INT,
     user_id INT,
-    branch_id INT,
+    vendor_id INT,
     quantity INT,
     unit_price DECIMAL(10, 2),
     total_price DECIMAL(10, 2),
@@ -147,7 +147,7 @@ create table sales_logs (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id),   
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (branch_id) REFERENCES branchs(branch_id)
+    FOREIGN KEY (vendor_id) REFERENCES vendors(vendor_id)
 );
 GO
 
@@ -168,8 +168,8 @@ GO
 create table messages (
     message_id INT IDENTITY(1,1) primary key,
     customer_id INT,
-    complain_topic varchar(50),
-    feedback varchar(50),
+    complain_topic nvarchar(50),
+    feedback nvarchar(50),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 GO
